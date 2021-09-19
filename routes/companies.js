e503edff811c46259a9f8c6adc8ccb68ee9ead25
zataara <bryantmac@gmail.com>
2021-09-19 01:46:35 -0700

    added get route for companies

---
+++ routes/companies.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        const results = await db.query(`SELECT * FROM companies`);
+        return res.json({ companies: results.rows })
+    } catch(e){
+        return next(e)
+    }
+})
+
+
+module.exports = router
