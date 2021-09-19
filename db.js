809638a4eced753df9230703788d849df388338f
zataara <bryantmac@gmail.com>
2021-09-19 01:47:15 -0700

    added db logic for test and development environments

--- db.js
+++ db.js
@@ -1,2 +1,21 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+    console.log('testing')
+    DB_URI = "postresql:///biztime_test";
+} else {
+    console.log('not testing')
+    DB_URI = "postgresql:///biztime"
+}
+
+let db = new Client({
+    connectionsString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
