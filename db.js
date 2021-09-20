a0415f816ad91dc162f11fa1ec2cada3381a3dec
zataara <bryantmac@gmail.com>
2021-09-20 10:18:17 -0700

    fixed typo in DB_URI variable

--- db.js
+++ db.js
@@ -2,20 +2,11 @@
 
 const { Client } = require("pg");
 
-let DB_URI;
-
-if (process.env.NODE_ENV === "test") {
-    console.log('testing')
-    DB_URI = "postresql:///biztime_test";
-} else {
-    console.log('not testing')
-    DB_URI = "postgresql:///biztime"
-}
-
-let db = new Client({
-    connectionsString: DB_URI
+const client = new Client({
+  connectionString: "postgresql:///biztime"
 });
 
-db.connect();
+client.connect();
 
-module.exports = db;
+
+module.exports = client;
