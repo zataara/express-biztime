985f6456fe521c250ce0b4a539e3be281e09a993
zataara <bryantmac@gmail.com>
2021-09-20 11:54:50 -0700

    updated dependencies

--- db.js
+++ db.js
@@ -2,11 +2,20 @@
 
 const { Client } = require("pg");
 
-const client = new Client({
-  connectionString: "postgresql:///biztime"
+let DB_URI;
+
+// If we're running in test "mode", use our test db
+// Make sure to create both databases!
+if (process.env.NODE_ENV === "test") {
+    DB_URI = "postgresql:///usersdb_test";
+} else {
+    DB_URI = "postgresql:///usersdb";
+}
+
+let db = new Client({
+    connectionString: DB_URI
 });
 
-client.connect();
-
+db.connect();
 
 module.exports = client;
