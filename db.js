862f14c11e2fa546826425c55a3937d07d867cfc
zataara <bryantmac@gmail.com>
2021-09-29 20:45:58 -0700

    fixing syntax issues

--- db.js
+++ db.js
@@ -1,21 +1,13 @@
 /** Database setup for BizTime. */
 
+
 const { Client } = require("pg");
 
-let DB_URI;
-
-// If we're running in test "mode", use our test db
-// Make sure to create both databases!
-if (process.env.NODE_ENV === "test") {
-    DB_URI = "postgresql:///usersdb_test";
-} else {
-    DB_URI = "postgresql:///usersdb";
-}
-
-let db = new Client({
-    connectionString: DB_URI
+const client = new Client({
+    connectionString: "postgresql:///biztime"
 });
 
-db.connect();
+client.connect();
+
 
 module.exports = client;
